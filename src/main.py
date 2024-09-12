b43d6567059c92d4dd0f34c392a5202d00591117
Kirill <kirill.ponomarenko2004@gmail.com>
2024-09-12 19:30:27 +0300

    #chore: add a basic project structure

---
+++ src/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI(root_path='/api/v1')
+
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
